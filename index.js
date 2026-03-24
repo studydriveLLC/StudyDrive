755f124877dae8e8e92b7291a1562616a85996f6
baticlean <baticlean225@gmail.com>
2026-03-24 16:45:28 +0000

    reparation anulation action

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { registerRootComponent } from 'expo';
 
+// Ajout du correctif pour les annulations de requêtes (Polyfill)
+if (typeof AbortSignal !== 'undefined' && typeof AbortSignal.abort !== 'function') {
+  AbortSignal.abort = function (reason) {
+    const controller = new AbortController();
+    controller.abort(reason);
+    return controller.signal;
+  };
+}
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
